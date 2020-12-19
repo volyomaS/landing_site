53913a009cf1da1e913038c43eb8542a9ff3e0a3
Daniil <37997765+volyomaS@users.noreply.github.com>
2020-12-19 22:27:06 +0300

    edit pop modal

--- static/js/pop.js
+++ static/js/pop.js
@@ -1,10 +1,7 @@
 function pop() {
     let modal = document.getElementById('pop_div');
-    let btn = document.getElementById('santasale');
     let span = document.getElementsByClassName("buttonClose")[0];
-    btn.onclick = function () {
-        modal.style.display = "block";
-    }
+    modal.style.display = "block";
     span.onclick = function () {
         modal.style.display = "none";
     }
