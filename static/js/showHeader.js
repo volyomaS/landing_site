520486895fadb2bfe10bb49d2521d59c33097032
Arseny.Polezhaev <arseny.polezhaev@gmail.com>
2020-12-21 23:14:02 +0300

    Added contacts and details pages

---
+++ static/js/showHeader.js
@@ -0,0 +1,10 @@
+const header = document.createElement("header")
+header.innerHTML = "<nav>\n" +
+    "                   <a class=\"main_nav\" href='\index.html\'>квадраты_инк</a> |\n" +
+    "                   <a class=\"main_nav\" href='\details.html'>История</a> |\n" +
+    "                   <a class=\"main_nav\" href='\contacts.html'>Контакты</a>\n" +
+    "               </nav>\n" +
+    "               <div id=\"header_pic\">\n" +
+    "                   <h1 id=\"welcome\">Лучшие_в_мире_квадраты</h1>\n" +
+    "               </div>"
+document.body.appendChild(header);
