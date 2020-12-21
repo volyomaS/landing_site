dab6bf9192bb3d6043e5ee3d0742167e6610013d
Arseny.Polezhaev <arseny.polezhaev@gmail.com>
2020-12-21 20:49:05 +0300

    Added feedback form

---
+++ static/js/feedback.js
@@ -0,0 +1,5 @@
+function onSend() {
+    document.getElementById("sent").style.display = "block"
+    document.getElementById("send-button").style.display = "none"
+    document.getElementById("input-area").style.display = "none"
+}
